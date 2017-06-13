4d4fccfad0c6ca58f43842af3911d6ffdb193591
Rohit Ukirde <ukirderohit@gmail.com>
2017-06-12 21:37:09 -0500

    ASSIGNMENT 2 COMPLETE

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -72,4 +72,115 @@ angular.module('ConfusionApp', []).controller('MenuController', ['$scope',functi
   $scope.toggleDetails = function() {
     $scope.showDetails = !$scope.showDetails;
 };
-}]);
+}])
+
+.controller('ContactController', ['$scope', function($scope) {
+
+            $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
+            
+            var channels = [{value:"tel", label:"Tel."}, {value:"Email",label:"Email"}];
+            
+            $scope.channels = channels;
+            $scope.invalidChannelSelection = false;
+                        
+        }])
+
+        .controller('FeedbackController', ['$scope', function($scope) {
+            
+            $scope.sendFeedback = function() {
+                
+                console.log($scope.feedback);
+                
+                if ($scope.feedback.agree && ($scope.feedback.mychannel == "")) {
+                    $scope.invalidChannelSelection = true;
+                    console.log('incorrect');
+                }
+                else {
+                    $scope.invalidChannelSelection = false;
+                    $scope.feedback = {mychannel:"", firstName:"", lastName:"", agree:false, email:"" };
+                    $scope.feedback.mychannel="";
+                    $scope.feedbackForm.$setPristine();
+                    console.log($scope.feedback);
+                }
+            };
+        }])
+
+        
+  .controller('dishDetailController', ['$scope', function ($scope) {
+
+        var dish = {
+            name: 'Uthapizza',
+            image: 'images/uthapizza.png',
+            category: 'mains',
+            label: 'Hot',
+            price: '4.99',
+            description: 'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.',
+            comments: [
+                {
+                    rating: 5,
+                    comment: "Imagine all the eatables, living in conFusion!",
+                    author: "John Lemon",
+                    date: "2012-10-16T17:57:28.556094Z"
+                },
+                {
+                    rating: 4,
+                    comment: "Sends anyone to heaven, I wish I could get my mother-in-law to eat it!",
+                    author: "Paul McVites",
+                    date: "2014-09-05T17:57:28.556094Z"
+                },
+                {
+                    rating: 3,
+                    comment: "Eat it, just eat it!",
+                    author: "Michael Jaikishan",
+                    date: "2015-02-13T17:57:28.556094Z"
+                },
+                {
+                    rating: 4,
+                    comment: "Ultimate, Reaching for the stars!",
+                    author: "Ringo Starry",
+                    date: "2013-12-02T17:57:28.556094Z"
+                },
+                {
+                    rating: 2,
+                    comment: "It's your birthday, we're gonna party!",
+                    author: "25 Cent",
+                    date: "2011-12-02T17:57:28.556094Z"
+                }
+
+            ]
+        };
+
+        $scope.dish = dish;
+
+    }]).controller('DishCommentController',['$scope',function($scope){
+            $scope.commentSec = { 
+                                author: '',
+                                rating: 5,
+                                comment: '',
+                                date: new Date().toISOString() 
+                              };
+            
+            //Step 1: Create a JavaScript object to hold the comment from the form
+            
+            $scope.submitComment = function () {
+                
+                //Step 2: This is how you record the date
+                // "The date property of your JavaScript object holding the comment" = new Date().toISOString();
+              $scope.date = new Date().toISOString();
+                
+                // Step 3: Push your comment into the dish's comment array
+                //$scope.dish.comments.push("Your JavaScript Object holding the comment");
+                $scope.dish.comments.push({
+                  author: $scope.commentSec.author,
+                  rating: $scope.commentSec.rating,
+                  comment: $scope.commentSec.comment,
+                  date: $scope.commentSec.date
+                });
+                
+                //Step 4: reset your form to pristine
+                $scope.commentSec.author = '';
+                $scope.commentSec.rating = 5;
+                $scope.commentSec.comment = '';
+                $scope.commentForm.$setPristine();
+            };
+    }]);
